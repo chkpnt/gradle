4515815e8073d8ead762b004640dbc0ba75300b1
Paul Merlin <paul@gradle.com>
2020-04-12 19:36:43 +0200

    Tests for finalizedBy/mustRunAfter/shouldRunAfter with instant execution
    mustRunAfter seems to work already
    
    finalizedBy and shouldRunAfter are also captured beause the implicit
    task dependencies are reflected in the task graph
    
    BUT the parallel scheduling is wrong and exhibited by the tests
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/instant-execution/src/integTest/groovy/org/gradle/instantexecution/InstantExecutionTaskExecutionIntegrationTest.groovy
+++ subprojects/instant-execution/src/integTest/groovy/org/gradle/instantexecution/InstantExecutionTaskExecutionIntegrationTest.groovy
@@ -16,7 +16,14 @@
 
 package org.gradle.instantexecution
 
+import org.gradle.test.fixtures.server.http.BlockingHttpServer
+import org.junit.Rule
+
 class InstantExecutionTaskExecutionIntegrationTest extends AbstractInstantExecutionIntegrationTest {
+
+    @Rule
+    BlockingHttpServer server = new BlockingHttpServer()
+
     def "honors task up-to-date spec"() {
         buildFile << """
             abstract class TaskWithComplexInputs extends DefaultTask {
@@ -49,5 +56,138 @@ class InstantExecutionTaskExecutionIntegrationTest extends AbstractInstantExecut
         then:
         result.assertTaskSkipped(":always")
         result.assertTasksNotSkipped(":never")
+    }
+
+    def "honors task finalizedBy"() {
+
+        def instant = newInstantExecutionFixture()
+        server.start()
+
+        given:
+        buildFile << """
+            task a {
+                doLast { ${server.callFromBuild("a")} }
+            }
+            task b {
+                finalizedBy a
+                doLast {
+                    Thread.sleep(500) // In order to exhibit the problem without flakiness
+                    ${server.callFromBuild("b")}
+                }
+            }
+        """
+
+        when:
+        server.expectConcurrent('b')
+        server.expectConcurrent('a')
+        instantRun 'b', '--parallel'
+
+        then:
+        instant.assertStateStored()
+
+        when:
+        server.expectConcurrent('b')
+        server.expectConcurrent('a')
+        instantRun 'b'
+
+        then:
+        instant.assertStateLoaded()
+
+        cleanup:
+        server.stop()
+    }
+
+    def "honors task mustRunAfter"() {
+
+        def instant = newInstantExecutionFixture()
+        server.start()
+
+        given:
+        buildFile << """
+            task a {
+                doLast { ${server.callFromBuild("a")} }
+            }
+            task b {
+                doLast { ${server.callFromBuild("b")} }
+            }
+            task c(dependsOn: ['a', 'b']) {
+                doLast { ${server.callFromBuild("c")} }
+            }
+            task d {
+                doLast { ${server.callFromBuild("d")} }
+            }
+            c.mustRunAfter d
+        """
+
+        when:
+        server.expectConcurrent("d")
+        server.expectConcurrent("a")
+        server.expectConcurrent("b")
+        server.expectConcurrent("c")
+        instantRun 'c', 'd'
+
+        then:
+        instant.assertStateStored()
+
+        when:
+        server.expectConcurrent("d", "a", "b")
+        server.expectConcurrent("c")
+        instantRun 'c', 'd'
+
+        then:
+        instant.assertStateLoaded()
+
+        cleanup:
+        server.stop()
+    }
+
+    def "honors task shouldRunAfter"() {
+
+        def instant = newInstantExecutionFixture()
+        server.start()
+
+        given:
+        buildFile << """
+            task a() {
+                dependsOn 'b'
+                doLast { ${server.callFromBuild("a")} }
+            }
+            task b() {
+                shouldRunAfter 'c'
+                doLast { ${server.callFromBuild("b")} }
+            }
+            task c() {
+                doLast {
+                    Thread.sleep(500) // In order to exhibit the problem without flakiness
+                    ${server.callFromBuild("c")}
+                }
+            }
+            task d() {
+                dependsOn 'c'
+                doLast { ${server.callFromBuild("d")} }
+            }
+        """
+
+        when:
+        server.expectConcurrent('c')
+        server.expectConcurrent('b')
+        server.expectConcurrent('a')
+        server.expectConcurrent('d')
+        instantRun 'a', 'd'
+
+        then:
+        instant.assertStateStored()
+
+        when:
+        server.expectConcurrent("c")
+        server.expectConcurrent("b")
+        server.expectConcurrent("a", "d")
+        instantRun 'a', 'd'
+
+        then:
+        instant.assertStateLoaded()
+
+        cleanup:
+        server.stop()
     }
 }
